9353863c9b0345d7821dc892a0110166707faa5b
TinhNguyenTB <tinh271103@gmail.com>
2024-09-07 16:26:22 +0700

    Configure Shared session state

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const TopBar = () => {
+    const { data: session } = useSession()
+    console.log(session)
+    if (session) {
+        return (
+            <>
+                Signed in as {session?.user?.email} <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        )
+    }
     return (
-        <div>TopBar</div>
+        <div>
+            TopBar
+            <button onClick={() => signIn()}>Sign in</button>
+        </div>
+
     )
 }
 
